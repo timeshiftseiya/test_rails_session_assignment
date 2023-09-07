edcc092547548ec56b95c99101dd2a5751b42f06
宮岡聖也 <miyaoka@diveintocode.jp>
2023-09-07 20:15:02 +0900

    add: specify version

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  Webdrivers::Chromedriver.required_version = "114.0.5735.90"
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
